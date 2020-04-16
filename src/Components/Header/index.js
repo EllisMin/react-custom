2621bd9871795221b836a48dc947e37503c07e7c
Ellis Min <dbmin@hotmail.com>
2020-04-16 05:13:55 -0500

    [ADD]: SEO

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import PropTypes from "prop-types";
 
 import "./styles.scss";
 
 const Header = ({ title }) => {
   return (
-    <div className="main-header">
+    <header className="main-header">
       <div className="main-header-inner">
         {title && <h1 style={{ fontSize: "1.5rem" }}>{`${title}`}</h1>}
       </div>
-    </div>
+    </header>
   );
 };
 
---
+++ src/Components/SEO/index.js
@@ -0,0 +1,77 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Helmet } from "react-helmet";
+
+function SEO({ metaDescription, lang, meta, title, author }) {
+  return (
+    <>
+      <Helmet
+        htmlAttributes={{
+          lang,
+        }}
+        title={title}
+        titleTemplate={`%s | ${title}`}
+        meta={[
+          {
+            name: `keywords`,
+            content: ``,
+          },
+          {
+            name: `description`,
+            content: metaDescription,
+          },
+          {
+            property: `og:title`,
+            content: title,
+          },
+          {
+            property: `og:description`,
+            content: metaDescription,
+          },
+          {
+            property: `og:type`,
+            content: `website`,
+          },
+          {
+            name: `twitter:card`,
+            content: `summary`,
+          },
+          {
+            name: `twitter:creator`,
+            content: author,
+          },
+          {
+            name: `twitter:title`,
+            content: title,
+          },
+          {
+            name: `twitter:description`,
+            content: metaDescription,
+          },
+        ].concat(meta)}
+      />
+      <Helmet>
+        {/* Google Font: Roboto */}
+        <link
+          href="https://fonts.googleapis.com/css?family=Roboto&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Helmet>
+    </>
+  );
+}
+
+SEO.defaultProps = {
+  lang: `en`,
+  meta: [],
+  description: ``,
+};
+
+SEO.propTypes = {
+  description: PropTypes.string,
+  lang: PropTypes.string,
+  meta: PropTypes.arrayOf(PropTypes.object),
+  title: PropTypes.string.isRequired,
+};
+
+export default SEO;
