6f05a6037baad7e5f09452f79f1918e32943023b
Ellis Min <dbmin@hotmail.com>
2020-04-15 23:09:46 -0500

    [FIX]: fixed custom alert displaying error message

--- src/Components/Alert/index.js
+++ src/Components/Alert/index.js
@@ -2,13 +2,13 @@ import React from "react";
 
 import "./styles.scss";
 
-const Alert = ({ message, error, onClose }) => {
+const Alert = ({ message, error, danger, onClose }) => {
   return (
     <>
-      {message && (
-        <div className={`${error ? "alert-error" : ""} alert-custom`}>
+      {(message || error) && (
+        <div className={`${danger ? "alert-error" : ""} alert-custom`}>
           <div />
-          <p>{message}</p>
+          {error ? <p>{error.message}</p> : <p>{message}</p>}
           {onClose && (
             <button className="close-custom" onClick={onClose}>
               <span>×</span>
